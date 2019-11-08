bb551507c935605a3a3713f8539ce58e61165d03
jiyoung <ji9310@naver.com>
2019-11-08 19:05:25 +0900

    메인카드뷰 그리기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -83,6 +83,7 @@ class _HomeScreenState extends State<HomeScreen>
       itemBuilder: (context, index) => _cardWidget(width * 0.8, height * 0.6, index),
       scrollDirection: Axis.vertical,
       onIndexChanged: _changeIndex,
+      loop: false,
     );
   }
 
@@ -91,7 +92,94 @@ class _HomeScreenState extends State<HomeScreen>
       child: Container(
         width: cwidth,
         height: cheight,
-        child: Center(child: Text('Card $index')),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+                top: 12.0,
+                right: 17.0,
+                child: Image.asset(
+                    'assets/images/ic_menu_horizon.png',
+                    height: 30.0,
+                    width: 30.0
+                ),
+            ),
+            Positioned(
+              bottom: 55.0,
+              child: Container(
+                height: 2,
+                width: cwidth,
+                color: Color(0x0C3B464B),
+              ),
+            ),
+            Positioned(
+              bottom: 14.0,
+              left: 22.5,
+              right: 20.5,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      '청순한 호랑이',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Color(0xFF8799A3)
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Image.asset(
+                            'assets/images/ic_view_count.png',
+                            height: 30.0,
+                            width: 30.0
+                        ),
+                        Text(
+                          '15',
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Color(0xFF8799A3)
+                          ),
+                        ),
+                        Container(
+                          height: 13,
+                          width: 2,
+                          color: Color(0xFF8799A3),
+                        ),
+                        Image.asset(
+                            'assets/images/ic_comment_count.png',
+                            height: 30.0,
+                            width: 30.0
+                        ),
+                        Text(
+                          '15',
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Color(0xFF8799A3)
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ),
+            Center(
+              child: Text(
+                '스타벅스 이번달 신메뉴 아보카도 블랜드 어때?',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontFamily: 'BlackHanSans'
+                ),
+              ),
+            ),
+          ],
+        ),
         decoration: BoxDecoration(
             color: Colors.white,
             shape: BoxShape.rectangle,
